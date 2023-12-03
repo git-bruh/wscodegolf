fe93eebb99f6b58593ac8258d12a1e32de4a7af4
git-bruh <e817509a-8ee9-4332-b0ad-3a6bdf9ab63f@aleeas.com>
2023-12-03 22:24:24 +0530

    sys: mvp using syscall wrappers

--- sys/main.go
+++ sys/main.go
@@ -5,9 +5,10 @@ import (
 	"unsafe"
 )
 
-// tinygo build -no-debug -scheduler=none -gc=none -panic=trap
+// tinygo build -no-debug -scheduler=none -gc=none -panic=trap -target=spec.json
+// strip -s -R .comment -R .note -R .eh_frame sys
 
-var buffer [1024]byte
+var buffer [2048]byte
 var used uintptr = 0
 
 //go:linkname alloc runtime.alloc
@@ -20,6 +21,37 @@ func alloc(size uintptr, layoutPtr unsafe.Pointer) unsafe.Pointer {
 }
 
 func main() {
-	var str = []byte{'h', 'i', '\n'}
-	syscall.Syscall(1, 1, uintptr(unsafe.Pointer(&str[0])), 3)
+	var httpInitMsg = []byte("GET /echo HTTP/1.1\r\nHost: localhost.com:8080\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\nSec-WebSocket-Version: 13\r\nConnection: keep-alive, Upgrade\r\nSec-Fetch-Mode: websocket\r\n\r\n")
+	var packet = []byte{
+		0b10000001, // FIN, RSV1, RSV2, RSV3, OpCode
+		0b10000101, // Mask Bit (Compulsary for client to set) + Payload
+		// NOTE: We don't need to set extended payload bits if our
+		// msg is less than 126 length
+		0b00000001,
+		0b00000010,
+		0b00000011,
+		0b00000100, // Mask
+		0b01101001,
+		0b01100111,
+		0b01101111,
+		0b01101000,
+		0b01101110, // Payload
+	}
+	var response [135]byte
+
+	var sa = syscall.SockaddrInet4{
+		Port: 8080,
+		Addr: [4]byte{127, 0, 0, 1},
+	}
+
+	var sock, _ = syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
+	syscall.Connect(sock, &sa)
+
+	syscall.Sendto(sock, httpInitMsg, 0, nil)
+	var n, _, _ = syscall.Recvfrom(sock, response[:], 0)
+	syscall.Sendto(sock, packet, 0, nil)
+	syscall.Recvfrom(sock, response[n:], 0)
+
+	syscall.Close(sock)
+	syscall.Write(1, response[:])
 }

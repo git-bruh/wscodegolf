e1e488f8f6e651dfc01e7ee5095ab5799d04e319
git-bruh <prathamIN@proton.me>
2024-01-21 00:15:24 +0530

    client: use syscall constants instead of hardcoding

--- client/syscalls_no_gc_custom_entry/main.go
+++ client/syscalls_no_gc_custom_entry/main.go
@@ -7,7 +7,7 @@ import (
 
 var (
 	httpInitMsg = []byte("GET / HTTP/1.1\r\nHost: dyte.io\r\nUpgrade:websocket\r\nConnection:Upgrade\r\nSec-WebSocket-Key:dGhlIHNhbXBsZSBub25jZQ==\r\nSec-WebSocket-Version:13\r\nConnection:Upgrade\r\n\r\n")
-	wsPayload = []byte{
+	wsPayload   = []byte{
 		0b10000001, // FIN, RSV1, RSV2, RSV3, OpCode
 		0b10000101, // Mask Bit (Compulsary for client to set) + Payload
 		// NOTE: We don't need to set extended payload bits if our
@@ -49,40 +49,40 @@ var (
 func main() {
 	// Create a IPv4 (AF_INET), TCP (SOCK_STREAM) socket FD
 	// __NR_socket, AF_INET, SOCK_STREAM
-	var sock, _, _ = syscall.Syscall(41, 0x2, 0x1, 0)
+	var sock, _, _ = syscall.Syscall(syscall.SYS_SOCKET, 0x2, 0x1, 0)
 
 	// Connect to the server using the `sockaddr_in` structure
 	// __NR_connect, fd, sockaddr_in, len(sockaddr_in)
-	syscall.Syscall6(42, sock, uintptr(unsafe.Pointer(&sockaddr[0])), uintptr(len(sockaddr)), 0, 0, 0)
+	syscall.Syscall6(syscall.SYS_CONNECT, sock, uintptr(unsafe.Pointer(&sockaddr[0])), uintptr(len(sockaddr)), 0, 0, 0)
 
 	// Send the HTTP message over the socket
 	// __NR_sendto, fd, buf, len(buf), flags, addr, addr_len
-	syscall.Syscall6(44, sock, uintptr(unsafe.Pointer(&httpInitMsg[0])), uintptr(len(httpInitMsg)), 0, 0, 0)
+	syscall.Syscall6(syscall.SYS_SENDTO, sock, uintptr(unsafe.Pointer(&httpInitMsg[0])), uintptr(len(httpInitMsg)), 0, 0, 0)
 
 	// Receive the response
 	// __NR_recvfrom, fd, buf, len(buf), flags, addr, addr_len
-	var n, _, _ = syscall.Syscall6(45, sock, uintptr(unsafe.Pointer(&response[0])), uintptr(len(response)), 0, 0, 0)
+	var n, _, _ = syscall.Syscall6(syscall.SYS_RECVFROM, sock, uintptr(unsafe.Pointer(&response[0])), uintptr(len(response)), 0, 0, 0)
 
 	// Send the WebSocket frame
 	// __NR_sendto
-	syscall.Syscall6(44, sock, uintptr(unsafe.Pointer(&wsPayload[0])), uintptr(len(wsPayload)), 0, 0, 0)
+	syscall.Syscall6(syscall.SYS_SENDTO, sock, uintptr(unsafe.Pointer(&wsPayload[0])), uintptr(len(wsPayload)), 0, 0, 0)
 
 	// Receive the response
 	// __NR_recvfrom
-	syscall.Syscall6(45, sock, uintptr(unsafe.Pointer(&response[n])), uintptr(len(response))-n, 0, 0, 0)
+	syscall.Syscall6(syscall.SYS_RECVFROM, sock, uintptr(unsafe.Pointer(&response[n])), uintptr(len(response))-n, 0, 0, 0)
 
 	// Close the socket FD
 	// __NR_close
-	syscall.Syscall(3, sock, 0, 0)
+	syscall.Syscall(syscall.SYS_CLOSE, sock, 0, 0)
 
 	// Write the response string to standard output
 	// __NR_write, STDOUT_FILENO
-	syscall.Syscall(1, 1, uintptr(unsafe.Pointer(&response[0])), uintptr(len(response)))
+	syscall.Syscall(syscall.SYS_WRITE, 1, uintptr(unsafe.Pointer(&response[0])), uintptr(len(response)))
 
 	// Cleanly exit the program with status code 0
 	// The libc does this for us in the usual flow, that goes like so:
 	//   __libc_start_main (libc) -> main (runtime_unix.go) -> main (main.go)
 	// But here, the entrypoint is in main.go itself
 	// __NR_exit, EXIT_SUCCESS
-	syscall.Syscall(60, 0, 0, 0)
+	syscall.Syscall(syscall.SYS_EXIT, 0, 0, 0)
 }
--- client/syscalls_only/main.go
+++ client/syscalls_only/main.go
@@ -45,33 +45,33 @@ func main() {
 
 	// Create a IPv4 (AF_INET), TCP (SOCK_STREAM) socket FD
 	// __NR_socket, AF_INET, SOCK_STREAM
-	var sock, _, _ = syscall.Syscall(41, 0x2, 0x1, 0)
+	var sock, _, _ = syscall.Syscall(syscall.SYS_SOCKET, 0x2, 0x1, 0)
 
 	// Connect to the server using the `sockaddr_in` structure
 	// __NR_connect, fd, sockaddr_in, len(sockaddr_in)
-	syscall.Syscall6(42, sock, uintptr(unsafe.Pointer(&sockaddr[0])), uintptr(len(sockaddr)), 0, 0, 0)
+	syscall.Syscall6(syscall.SYS_CONNECT, sock, uintptr(unsafe.Pointer(&sockaddr[0])), uintptr(len(sockaddr)), 0, 0, 0)
 
 	// Send the HTTP message over the socket
 	// __NR_sendto, fd, buf, len(buf), flags, addr, addr_len
-	syscall.Syscall6(44, sock, uintptr(unsafe.Pointer(&httpInitMsg[0])), uintptr(len(httpInitMsg)), 0, 0, 0)
+	syscall.Syscall6(syscall.SYS_SENDTO, sock, uintptr(unsafe.Pointer(&httpInitMsg[0])), uintptr(len(httpInitMsg)), 0, 0, 0)
 
 	// Receive the response
 	// __NR_recvfrom, fd, buf, len(buf), flags, addr, addr_len
-	var n, _, _ = syscall.Syscall6(45, sock, uintptr(unsafe.Pointer(&response[0])), uintptr(len(response)), 0, 0, 0)
+	var n, _, _ = syscall.Syscall6(syscall.SYS_RECVFROM, sock, uintptr(unsafe.Pointer(&response[0])), uintptr(len(response)), 0, 0, 0)
 
 	// Send the WebSocket frame
 	// __NR_sendto
-	syscall.Syscall6(44, sock, uintptr(unsafe.Pointer(&wsPayload[0])), uintptr(len(wsPayload)), 0, 0, 0)
+	syscall.Syscall6(syscall.SYS_SENDTO, sock, uintptr(unsafe.Pointer(&wsPayload[0])), uintptr(len(wsPayload)), 0, 0, 0)
 
 	// Receive the response
 	// __NR_recvfrom
-	syscall.Syscall6(45, sock, uintptr(unsafe.Pointer(&response[n])), uintptr(len(response))-n, 0, 0, 0)
+	syscall.Syscall6(syscall.SYS_RECVFROM, sock, uintptr(unsafe.Pointer(&response[n])), uintptr(len(response))-n, 0, 0, 0)
 
 	// Close the socket FD
 	// __NR_close
-	syscall.Syscall(3, sock, 0, 0)
+	syscall.Syscall(syscall.SYS_CLOSE, sock, 0, 0)
 
 	// Write the response string to standard output
 	// __NR_write, STDOUT_FILENO
-	syscall.Syscall(1, 1, uintptr(unsafe.Pointer(&response[0])), uintptr(len(response)))
+	syscall.Syscall(syscall.SYS_WRITE, 1, uintptr(unsafe.Pointer(&response[0])), uintptr(len(response)))
 }
